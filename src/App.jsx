aa2e5139a4ba379c0ff33313a531d2f4e5a0fdda
Paolo <landerospaolo@gmail.com>
2026-06-30 19:14:33 -0400

    feat: implement base Todo App functionality

---
+++ src/App.jsx
@@ -0,0 +1,67 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [tareas, setTareas] = useState([]);
+  const [texto, setTexto] = useState("");
+
+  const escucharInput = (e) => {
+    setTexto(e.target.value);
+  };
+
+  const agregarTarea = () => {
+    if (texto.trim()) {
+      console.log("Agregando tarea...");
+
+      const nuevaTarea = {
+        id: Date.now(),
+        texto: texto.trim(),
+        completada: false,
+      };
+
+      setTareas([...tareas, nuevaTarea]);
+
+      setTexto("");
+    } else {
+      console.log("Debes escribir una tarea valida");
+    }
+  };
+
+  const eliminarTarea = (id) => {
+    const nuevasTareas = tareas.filter(tarea => tarea.id !== id)
+    setTareas(nuevasTareas)
+  }
+
+  const cambiarEstado = (id) => {
+    setTareas(tareas.map(tarea => {
+      if (tarea.id === id) {
+        return {...tarea, completada: !tarea.completada}
+      }
+      return tarea
+    }))
+  }
+
+  console.log(tareas)
+
+  return (
+    <>
+      <h1>TodoApp</h1>
+      <input
+        type="text"
+        value={texto}
+        onChange={escucharInput}
+        placeholder="Escribe tu tarea"
+      />
+      <button onClick={agregarTarea}>Agregar</button>
+      {tareas.map((t) => (
+        <div key={t.id} style={{textDecoration: t.completada? 'line-through' : 'none'}}>
+          <input type="checkbox" id={t.id} name={t.texto} checked={t.completada} onChange={() => cambiarEstado(t.id)}/>
+          <label htmlFor={t.id}>{t.texto}</label>
+          <button onClick={() => eliminarTarea(t.id)}>Eliminar</button>
+        </div>
+      ))}
+    </>
+  );
+}
+
+export default App;
